640e39c46a0550e25f0482c353ddbd2cc1dde129
Muhammed Akgümüş <mmakgumus@gmail.com>
2018-01-15 17:45:17 +0300

    ViewPager eklendi.Plug ve Support Fragmentları eklendi.Main Activity dmüzenlendi

--- app/src/main/java/com/thesis/bmm/smartplug/activities/MainActivity.java
+++ app/src/main/java/com/thesis/bmm/smartplug/activities/MainActivity.java
@@ -9,11 +9,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+
 import com.thesis.bmm.smartplug.R;
+import com.thesis.bmm.smartplug.adapter.ViewPagerAdapter;
+import com.thesis.bmm.smartplug.fragments.PlugsFragment;
+import com.thesis.bmm.smartplug.fragments.SupportFragment;
 
 public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener, ViewPager.OnPageChangeListener {
     private TabLayout tabLayout = null;
     private ViewPager vpFragments = null;
+    private Toolbar toolbar = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -21,13 +27,31 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        initView();
     }
 
     private void initView() {
-
+        tabLayout = findViewById(R.id.activity_main_tablayout);
+        vpFragments = findViewById(R.id.activity_main_vpFragments);
+        toolbar = findViewById(R.id.toolbar);
+        initEvent();
     }
+
+    private void initEvent() {
+        setSupportActionBar(toolbar);
+        tabLayout.setupWithViewPager(vpFragments);
+        tabLayout.addOnTabSelectedListener(this);
+        ViewPagerAdapter viewpagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        PlugsFragment plugsFragment = new PlugsFragment();
+        SupportFragment supportFragment = new SupportFragment();
+        viewpagerAdapter.addFragment(plugsFragment, "Prizler");
+        viewpagerAdapter.addFragment(supportFragment, "Destek");
+        vpFragments.setAdapter(viewpagerAdapter);
+        vpFragments.addOnPageChangeListener(this);
+        vpFragments.setCurrentItem(0);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
